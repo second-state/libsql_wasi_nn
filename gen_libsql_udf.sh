03a16fa73ad7d84b7e94f92ace401fc720e738f9
csh <458761603@qq.com>
2023-03-20 18:12:50 +0800

    bug recurrence

--- gen_libsql_udf.sh
+++ gen_libsql_udf.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+
+cargo wasi build --release
+
+wasmedgec target/wasm32-wasi/release/wasi_nn_udf.wasm wasi_nn_udf.aot.wasm
+
 FUNC_NAME='classify'
 echo "DROP FUNCTION IF EXISTS ${FUNC_NAME};" > create_${FUNC_NAME}_udf.sql
 echo -n "CREATE FUNCTION ${FUNC_NAME} LANGUAGE wasm AS X'" >> create_${FUNC_NAME}_udf.sql
-xxd -p target/wasm32-wasi/release/wasi_nn_udf.wasm | tr -d "\n" >> create_${FUNC_NAME}_udf.sql 
+xxd -p wasi_nn_udf.aot.wasm | tr -d "\n" >> create_${FUNC_NAME}_udf.sql 
 echo "';" >> create_${FUNC_NAME}_udf.sql
